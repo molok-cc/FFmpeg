d4efc31a45aa20a87f8581a97fc046d8f596cb52
Wiki Wang <wikiw@molok.cc>
2022-09-05 22:50:19 +0800

    fix msbuild arg prefix

--- scripts/win32/install.sh
+++ scripts/win32/install.sh
@@ -14,7 +14,7 @@ cp -r amf/public/include /usr/local/include/AMF
 cd $VENDOR/github.com/Intel-Media-SDK/MediaSDK
 cd api/mfx_dispatch/windows
 sed -i 's/10.0.17134.0/10.0.22621.0/' libmfx_vs2015.vcxproj
-msbuild libmfx_vs2015.vcxproj /p:PlatformToolset=v143 /p:Configuration=Release
+MSBuild.exe libmfx_vs2015.vcxproj -p:PlatformToolset=v143 -p:Configuration=Release
 cd ../../..
 cp -r api/include /usr/local/include/mfx
 cp ../build/win_x64/Release/lib/libmfx_vs2015.lib /usr/local/lib/libmfx.lib
