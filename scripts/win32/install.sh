a2c2f1e1b7201450157c4683768297ce19d5adcf
Wiki Wang <wikiw@molok.cc>
2022-09-16 14:15:00 +0800

    fix dav1d vscrt

--- scripts/win32/install.sh
+++ scripts/win32/install.sh
@@ -19,13 +19,15 @@ rm /usr/bin/link.exe
 
 cd $VENDOR/code.videolan.org/videolan/dav1d
 mkdir build && cd build
-configure="meson --prefix $PREFIX .."
+configure="meson .. --prefix=$PREFIX -Denable_tests=false -Denable_tools=false"
 if [[ $TRIPLET == *-static ]]; then
-  configure+=" --default-library=static"
+  configure+=" --default-library=static -Db_vscrt=static_from_buildtype"
 fi
+configure+=" -Dbuildtype=release"
 eval $configure
 meson compile
 meson install
+mv $PREFIX/lib/libdav1d.a $PREFIX/lib/dav1d.lib
 
 cd $VENDOR/github.com/FFmpeg/nv-codec-headers
 make install
