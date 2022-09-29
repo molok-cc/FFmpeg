1a424df609629296ba6682283934c6394c295cdd
Wiki Wang <wikiw@molok.cc>
2022-09-29 15:27:37 +0800

    build android

--- scripts/android/build.sh
+++ scripts/android/build.sh
@@ -13,16 +13,16 @@ ARCH=aarch64
 TARGET=$ARCH-linux-android33
 
 CC=$ANDROID_NDK/toolchains/llvm/prebuilt/$HOST_TAG/bin/$TARGET-clang
-CXX=$ANDROID_NDK/toolchains/llvm/prebuilt/$HOST_TAG/bin/$TARGET-clang++
 
 cd $ROOT
 ./configure --prefix=$PREFIX \
   --enable-gpl --enable-version3 --enable-nonfree \
+  --enable-jni --enable-mediacodec \
+  --enable-small --disable-runtime-cpudetect \
   --enable-cross-compile \
   --arch=$ARCH \
   --target-os=android \
   --cc=$CC \
-  --cxx=$CXX \
   --extra-cflags="-I$PREFIX/include -fvisibility=hidden" \
   --extra-ldflags="-L$PREFIX/lib" \
   --enable-pic
