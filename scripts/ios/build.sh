ae53129126ee8da98dbb6128b765ca288ad35574
Wiki Wang <wikiw@molok.cc>
2022-11-02 17:13:18 +0800

    use realpath from homebrew coreutils

--- scripts/ios/build.sh
+++ scripts/ios/build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-realpath() {
-  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 if [ -z "$TRIPLET" ]; then
@@ -10,20 +7,24 @@ if [ -z "$TRIPLET" ]; then
 fi
 ROOT=`realpath $SCRIPTPATH/../..`
 PREFIX=$ROOT/build/$TRIPLET
+LIBDIR=$PREFIX/lib
 
 SDK_NAME=iphoneos
 ARCH=arm64
 
 CC="xcrun -sdk $SDK_NAME clang"
 CFLAGS="-I$PREFIX/include -fvisibility=hidden"
-LDFLAGS="-L$PREFIX/lib"
+LDFLAGS="-L$LIBDIR"
+
+mkdir -p $LIBDIR
 
 cd $ROOT
 ./configure --prefix=$PREFIX \
   --enable-gpl --enable-version3 --enable-nonfree \
   --enable-small --disable-runtime-cpudetect \
-  --enable-cross-compile \
+  --disable-audiotoolbox \
   --arch=$ARCH \
+  --enable-cross-compile \
   --target-os=darwin \
   --cc="$CC" \
   --extra-cflags="$CFLAGS" \
@@ -32,5 +33,5 @@ cd $ROOT
 
 cat ffbuild/config.log
 
-make -j
-make install
+# make -j
+# make install
--- scripts/ios/install.sh
+++ scripts/ios/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 sudo xcode-select -switch /Applications/Xcode_14.0.1.app/Contents/Developer
+
+brew install coreutils
